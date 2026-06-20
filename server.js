229b304e9feff8f79ea01912e8501aa561ecc993
Divya Jyothi <divyajyothi.bonam@gmail.com>
2026-06-20 21:28:37 +0530

    Added enterprise login and signup page

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import express from "express";
-const app=express();
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
 const port = 3000;
-const host = 'localhost';
-app.listen(port,host,()=>{
-    console.log(`http://${host}:${port}`);
-})
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+
+app.get("/login", (req, res) => {
+    res.render("pages/login");
+});
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
